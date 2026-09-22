a4dbf673a4d8a1bebf78c6a046a4dcfd122256b4
Rafael Mota Gonçalves <rafaelmotagoncalves7@gmail.com>
2026-09-22 20:11:48 +0000

    deixando completo

---
+++ meu-projeto/src/App.jsx
@@ -0,0 +1,23 @@
+import ElectricBorder from "./components/ElectricBorder";
+function App() {
+return (
+<div style={{
+display: 'flex',
+justifyContent: 'center',
+alignItems: 'center',
+height: '100vh'
+}}>
+<ElectricBorder
+color="#5227FF"
+speed={1}
+chaos={0.5}
+borderRadius={16}
+>
+<div style={{ padding: '40px', fontSize: '24px', color: 'white' }}>
+Efeito Electric Border
+</div>
+</ElectricBorder>
+</div>
+)
+}
+export default App
